af6b1dfcc22d9db20af66dcfbb21f0c1aa6582fa
ikhsanytc <ikhsanm181209@gmail.com>
2025-02-26 22:51:23 +0700

    create page login, tapi belum nambahin apa apa sih wkwk

---
+++ app/login/clientPage.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const ClientPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white"></div>
+  );
+};
+
+export default ClientPage;
